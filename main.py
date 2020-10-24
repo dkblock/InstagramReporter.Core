cd4a9642220137c3be0141df9387d26fea6334a1
Iurii Kazanov <iurii.kazanov@akvelon.com>
2020-10-24 23:30:07 +0300

    Refactor code

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import sys
 import importlib
+import sys
+
+from handlers import error_handler
 
 
+@error_handler
 def main():
-    number = ''
-    try:
-        number = sys.argv[1]
-        if int(number) > 5 or int(number) < 1:
-            print('Номер задачи находится в диапозоне от 1 до 5')
-    except IndexError:
-        sys.exit("Чтобы запустить приложение используйте комманду: 'python3 main.py <номер задания>'")  # noqa: E501
-    importlib.import_module(f'tasks.{number}').main()
+    number = sys.argv[1]
+    importlib.import_module(f'tasks.task_{number}').main()
 
 
 if __name__ == "__main__":
